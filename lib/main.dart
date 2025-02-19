cd1803508525ca1257a35a66df30ef4a578179e3
Chaitanya Kumar Dewangan <chaitanyakumar1401@gmail.com>
2025-02-19 18:18:05 +0530

    form page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:go_router/go_router.dart';
+import 'package:virtualcard/models/contact_model.dart';
+import 'package:virtualcard/pages/form_page.dart';
 import 'package:virtualcard/pages/home_page.dart';
 import 'package:virtualcard/pages/scan_page.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -23,20 +25,26 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-
     );
   }
 
   final _router = GoRouter(debugLogDiagnostics: true, routes: [
     GoRoute(
-      name: HomePage.routeName,
-      path: HomePage.routeName,
-      builder: (context, state) => const HomePage(),
-      routes: [
-        GoRoute(name: ScanPage.routeName,
-        path: ScanPage.routeName,
-        builder: (context, state)=> const ScanPage())
-      ]
-    )
+        name: HomePage.routeName,
+        path: HomePage.routeName,
+        builder: (context, state) => const HomePage(),
+        routes: [
+          GoRoute(
+              name: ScanPage.routeName,
+              path: ScanPage.routeName,
+              builder: (context, state) => const ScanPage(),
+              routes: [
+                GoRoute(
+                  name: FormPage.routeName,
+                  path: FormPage.routeName,
+                  builder: (context, state) => FormPage(contactModel: state.extra! as ContactModel,),
+                )
+              ])
+        ])
   ]);
 }
--- lib/pages/scan_page.dart
+++ lib/pages/scan_page.dart
@@ -1,9 +1,15 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:virtualcard/models/contact_model.dart';
+import 'package:virtualcard/pages/form_page.dart';
+
+import '../utils/constants.dart';
 
 class ScanPage extends StatefulWidget {
   static const String routeName = 'scan';
@@ -15,14 +21,42 @@ class ScanPage extends StatefulWidget {
 }
 
 class _ScanPageState extends State<ScanPage> {
-  bool ScanOver = false;
+  bool isScanOver = false;
   List<String> lines = [];
+  String name = '',
+      mobile = '',
+      email = '',
+      address = '',
+      company = '',
+      designation = '',
+      website = '',
+      image = '';
+
+  void createContact() {
+    final contact = ContactModel(
+      name: name,
+      mobile: mobile,
+      email: email,
+      address: address,
+      company: company,
+      designation: designation,
+      website: website,
+      image: image,
+    );
+    context.goNamed(FormPage.routeName, extra: contact);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Scan Page'),
+        actions: [
+          IconButton(
+            onPressed: image.isEmpty ? null : createContact,
+            icon: const Icon(Icons.arrow_forward),
+          )
+        ],
       ),
       body: ListView(
         children: [
@@ -45,6 +79,42 @@ class _ScanPageState extends State<ScanPage> {
               ),
             ],
           ),
+          if (isScanOver)
+            Card(
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    DragTargetItem(
+                        property: ContactProperties.name,
+                        onDrop: getPropertyValue),
+                    DragTargetItem(
+                        property: ContactProperties.mobile,
+                        onDrop: getPropertyValue),
+                    DragTargetItem(
+                        property: ContactProperties.email,
+                        onDrop: getPropertyValue),
+                    DragTargetItem(
+                        property: ContactProperties.company,
+                        onDrop: getPropertyValue),
+                    DragTargetItem(
+                        property: ContactProperties.designation,
+                        onDrop: getPropertyValue),
+                    DragTargetItem(
+                        property: ContactProperties.address,
+                        onDrop: getPropertyValue),
+                    DragTargetItem(
+                        property: ContactProperties.website,
+                        onDrop: getPropertyValue),
+                  ],
+                ),
+              ),
+            ),
+          if (isScanOver)
+            const Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(hint),
+            ),
           Wrap(
             children: lines.map((line) => LineItem(line: line)).toList(),
           )
@@ -58,6 +128,9 @@ class _ScanPageState extends State<ScanPage> {
       source: camera,
     );
     if (xFile != null) {
+      setState(() {
+        image = xFile.path;
+      });
       EasyLoading.show(status: 'Please Wait');
       print(xFile.path);
       final textRoconizer = TextRecognizer(script: TextRecognitionScript.latin);
@@ -72,9 +145,35 @@ class _ScanPageState extends State<ScanPage> {
       }
       setState(() {
         lines = templist;
-        ScanOver = true;
+        isScanOver = true;
       });
       print(templist);
+    }
+  }
+
+  getPropertyValue(String property, String value) {
+    switch (property) {
+      case ContactProperties.name:
+        name = value;
+        break;
+      case ContactProperties.mobile:
+        mobile = value;
+        break;
+      case ContactProperties.email:
+        email = value;
+        break;
+      case ContactProperties.company:
+        company = value;
+        break;
+      case ContactProperties.designation:
+        designation = value;
+        break;
+      case ContactProperties.address:
+        address = value;
+        break;
+      case ContactProperties.website:
+        website = value;
+        break;
     }
   }
 }
@@ -87,22 +186,91 @@ class LineItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LongPressDraggable(
-        data: line,
-        dragAnchorStrategy: childDragAnchorStrategy,
-        feedback: Container(
-          key: GlobalKey(),
-          padding: const EdgeInsets.all(8.0),
-          decoration: BoxDecoration(color: Colors.black45),
-          child: Text(
-            line,
-            style: Theme.of(context)
-                .textTheme
-                .titleMedium!
-                .copyWith(color: Colors.white),
-          ),
+      data: line,
+      dragAnchorStrategy: childDragAnchorStrategy,
+      feedback: Container(
+        key: GlobalKey(),
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(color: Colors.black45),
+        child: Text(
+          line,
+          style: Theme.of(context)
+              .textTheme
+              .titleMedium!
+              .copyWith(color: Colors.white),
         ),
-    child: Chip(
-      label: Text(line),
-    ),);
+      ),
+      child: Chip(
+        label: Text(line),
+      ),
+    );
+  }
+}
+
+class DragTargetItem extends StatefulWidget {
+  final String property;
+  final Function(String, String) onDrop;
+
+  const DragTargetItem(
+      {super.key, required this.onDrop, required this.property});
+
+  @override
+  State<DragTargetItem> createState() => _DragTargetItemState();
+}
+
+class _DragTargetItemState extends State<DragTargetItem> {
+  String dragItem = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: Text(widget.property),
+        ),
+        Expanded(
+            flex: 2,
+            child: DragTarget<String>(
+              builder: (context, candidateData, rejectedData) => Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  border: candidateData.isNotEmpty
+                      ? Border.all(color: Colors.red, width: 2)
+                      : null,
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(dragItem.isEmpty ? 'Drop here' : dragItem),
+                    ),
+                    if (dragItem.isNotEmpty)
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            dragItem = '';
+                          });
+                        },
+                        child: const Icon(
+                          Icons.clear,
+                          size: 15,
+                        ),
+                      )
+                  ],
+                ),
+              ),
+              onAccept: (value) {
+                setState(() {
+                  if (dragItem.isNotEmpty) {
+                    dragItem = value;
+                  } else {
+                    dragItem += ' $value';
+                  }
+                });
+                widget.onDrop(widget.property, dragItem);
+              },
+            ))
+      ],
+    );
   }
 }
