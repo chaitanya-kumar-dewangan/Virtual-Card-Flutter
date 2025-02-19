413e0342bc0dc15d4eb1946d0e1089b259b886af
Chaitanya Kumar Dewangan <chaitanyakumar1401@gmail.com>
2025-02-19 09:44:21 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:virtualcard/pages/home_page.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      routerConfig: _router,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+
+    );
+  }
+
+  final _router = GoRouter(debugLogDiagnostics: true, routes: [
+    GoRoute(
+      name: HomePage.routeName,
+      path: HomePage.routeName,
+      builder: (context, state) => const HomePage(),
+    )
+  ]);
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  static const String routeName = '/';
+
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Contact List'),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          shape: const CircleBorder(),
+          child: const Icon(Icons.add),
+        ),
+        bottomNavigationBar: BottomAppBar(
+
+          padding: EdgeInsets.zero,
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 10,
+          clipBehavior: Clip.antiAlias,
+          child: BottomNavigationBar(
+              backgroundColor: Colors.blue.shade100,
+              onTap: (index){
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+              currentIndex: selectedIndex,
+              items: [
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'ALL'),
+            BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'FAVORITES'),
+          ]),
+
+        ));
+  }
+}
