b327a1ab38f8af9cabda1756163004a243a45664
Chaitanya Kumar Dewangan <chaitanyakumar1401@gmail.com>
2025-02-21 15:58:03 +0530

    contact model class

--- lib/models/contact_model.dart
+++ lib/models/contact_model.dart
@@ -1,4 +1,16 @@
-class ContactModel  {
+const String tableContact = 'tbl_contact';
+const String tblContactColId = 'id';
+const String tblContactColName = 'name';
+const String tblContactColMobile = 'mobile';
+const String tblContactColEmail = 'email';
+const String tblContactColAddress = 'address';
+const String tblContactColCompany = 'company';
+const String tblContactColDesignation = 'designation';
+const String tblContactColWebsite = 'website';
+const String tblContactColImage = 'image';
+const String tblContactColdFavorite = 'favorite';
+
+class ContactModel {
   int id;
   String name;
   String mobile;
@@ -22,4 +34,33 @@ class ContactModel  {
     this.image = '',
     this.favorite = false,
   });
+
+  Map<String, dynamic> tomap() {
+    final map = <String, dynamic>{
+      tblContactColName: name,
+      tblContactColMobile: mobile,
+      tblContactColEmail: email,
+      tblContactColAddress: address,
+      tblContactColCompany: company,
+      tblContactColDesignation: designation,
+      tblContactColWebsite: website,
+      tblContactColImage: image,
+      tblContactColdFavorite: favorite ? 1 :0,
+    };
+    if(id>0) {
+      map[tblContactColId] = id;
+    }
+    return map;
+  }
+  factory ContactModel.fromMap(Map<String, dynamic>map) => ContactModel(
+  name: map[tblContactColName],
+  mobile: map[tblContactColMobile],
+  email: map[tblContactColEmail],
+  address: map[tblContactColAddress],
+  company: map[tblContactColCompany],
+  designation: map[tblContactColDesignation],
+  website: map[tblContactColWebsite],
+  image: map[tblContactColImage],
+  favorite: map[tblContactColdFavorite] == 1 ? true: false,
+  );
 }
